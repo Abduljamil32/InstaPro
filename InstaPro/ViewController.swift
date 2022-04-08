38fac36660d0390022f38034efb787e778d6d3f8
co.abu <anizomxoja@gmail.com>
2022-04-08 18:11:59 +0500

    Initial Commit

---
+++ InstaPro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaPro
+//
+//  Created by Avaz Mukhitdinov on 08/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
